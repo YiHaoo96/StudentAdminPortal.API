8f4790cef02f673a1be56803ca28a61ffc5f6d8a
YiHaoo96 <leeyihao96@gmail.com>
2023-03-16 21:46:17 +0800

    Create Student Features

--- StudentAdminPortal.API/StudentAdminPortal.API/Controllers/StudentsController.cs
+++ StudentAdminPortal.API/StudentAdminPortal.API/Controllers/StudentsController.cs
@@ -30,7 +30,7 @@ namespace StudentAdminPortal.API.Controllers
         }
 
         [HttpGet]
-        [Route("[controller]/{studentId:guid}")]
+        [Route("[controller]/{studentId:guid}"), ActionName("GetStudentAsync")]
         public async Task<IActionResult> GetStudentAsync([FromRoute] Guid studentId)
         {
             var student = await studentRepository.GetStudentAsync(studentId);
@@ -63,6 +63,14 @@ namespace StudentAdminPortal.API.Controllers
 
             var student = await studentRepository.DeleteStudent(studentId);
             return Ok(mapper.Map<Student>(student));
+        }
+
+        [HttpPost]
+        [Route("[controller]/Add")]
+        public async Task<IActionResult> AddStudentAsync([FromBody] AddStudentRequest request)
+        {
+            var student = await studentRepository.AddStudent(mapper.Map<DataModels.Student>(request));
+            return CreatedAtAction(nameof(GetStudentAsync), new { studentId = student.Id }, mapper.Map<Student>(student));
         }
     }
 }
